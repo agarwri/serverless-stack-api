e4b397000da1f2fdb6118cb085a58d547f7e4b13
agarwri <ritika.agarwal1995@gmail.com>
2021-02-22 00:09:52 -0500

    declare providerUrl variable properly

--- infrastructure/lib/CognitoAuthRoles.js
+++ infrastructure/lib/CognitoAuthRoles.js
@@ -66,7 +66,7 @@ export default class CognitoAuthRoles extends cdk.Construct {
       })
     );
 
-    providerUrl = userPool.userPoolProviderUrl + ":" + userPoolClient.userPoolClientId;
+    const providerUrl = userPool.userPoolProviderUrl + ":" + userPoolClient.userPoolClientId;
 
     new cognito.CfnIdentityPoolRoleAttachment(
       this,
